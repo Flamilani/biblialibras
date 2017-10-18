523ec436381ecfb1d56cc6aabeef342820980b66
Flavio Milani <milanidesign7@gmail.com>
2017-10-18 00:42:09 -0200

    atualizando pagina inicial

--- meu_projeto/application/views/inc/footer.php
+++ meu_projeto/application/views/inc/footer.php
@@ -4,21 +4,21 @@
     <div class="footer-above">
         <div class="container">
             <div class="row">
-                <div class="footer-col col-md-4">
+              <!--   <div class="footer-col col-md-4">
                     <h3>Location</h3>
                     <p>3481 Melrose Place
                         <br>Beverly Hills, CA 90210</p>
-                </div>
-                <div class="footer-col col-md-4">
-                    <h3>Around the Web</h3>
+                </div> -->
+                <div class="footer-col">
+                    <h3>Redes Sociais</h3>
                     <ul class="list-inline">
                         <li>
-                            <a href="#" class="btn-social btn-outline"><span class="sr-only">Facebook</span><i class="fa fa-fw fa-facebook"></i></a>
+                            <a href="https://www.facebook.com/ABibliaEmLibras" target="_blank" class="social"><img src="assets/img/facebook.png"></a>
                         </li>
-                        <li>
-                            <a href="#" class="btn-social btn-outline"><span class="sr-only">Google Plus</span><i class="fa fa-fw fa-google-plus"></i></a>
+                       <li>
+                           <a href="#" class="social"><img src="assets/img/youtube.png"></a>
                         </li>
-                        <li>
+                        <!--  <li>
                             <a href="#" class="btn-social btn-outline"><span class="sr-only">Twitter</span><i class="fa fa-fw fa-twitter"></i></a>
                         </li>
                         <li>
@@ -26,13 +26,13 @@
                         </li>
                         <li>
                             <a href="#" class="btn-social btn-outline"><span class="sr-only">Dribble</span><i class="fa fa-fw fa-dribbble"></i></a>
-                        </li>
+                        </li> -->
                     </ul>
                 </div>
-                <div class="footer-col col-md-4">
+               <!--  <div class="footer-col col-md-4">
                     <h3>About Freelancer</h3>
                     <p>Freelance is a free to use, open source Bootstrap theme created by <a href="http://startbootstrap.com">Start Bootstrap</a>.</p>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
@@ -40,7 +40,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    Copyright &copy; Your Website 2016
+                    Todos os direitos autoriais &copy; A Bíblia em Libras - <?php echo date('Y'); ?>
                 </div>
             </div>
         </div>
--- meu_projeto/application/views/modals/modals.php
+++ meu_projeto/application/views/modals/modals.php
@@ -46,12 +46,12 @@
 
                 <form name="Inscricao" id="inscricaoForm">
                     <div class="row">
-                        <div class="form-group floating-label-form-group controls col-md-6">
+                        <div class="form-group floating-label-form-group controls col-md-6 col-lg-6">
                             <label for="nome">Nome Completo</label>
                             <input type="text" class="form-control" placeholder="Nome Completo" id="nome" required data-validation-required-message="Favor digite seu nome completo.">
                             <p class="help-block text-danger"></p>
                         </div>
-                        <div class="form-group floating-label-form-group controls col-md-6">
+                        <div class="form-group floating-label-form-group controls col-md-6 col-lg-6">
                             <label for="email">E-mail</label>
                             <input type="email" class="form-control" placeholder="E-mail" id="email" required data-validation-required-message="Favor digite seu e-mail.">
                             <p class="help-block text-danger"></p>
@@ -59,38 +59,72 @@
                     </div>
 
                     <div class="row">
-                        <div class="form-group floating-label-form-group controls col-md-6">
+                        <div class="form-group floating-label-form-group controls col-md-6 col-lg-6">
                             <label for="senha">Senha</label>
                             <input type="password" class="form-control" placeholder="Senha" id="senha" required data-validation-required-message="Favor digite sua senha.">
                             <p class="help-block text-danger"></p>
                         </div>
-                        <div class="form-group floating-label-form-group controls col-md-6">
+                        <div class="form-group floating-label-form-group controls col-md-6 col-lg-6">
                             <label for="confsenha">Confirme Senha</label>
                             <input type="password" class="form-control" placeholder="Confirme Senha" id="confsenha" required data-validation-required-message="Favor confirme sua senha.">
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group floating-label-form-group controls col-md-6">
+                        <div class="form-group floating-label-form-group controls col-md-6 col-lg-6">
                             <label for="email">Telefone</label>
                             <input type="tel" class="form-control" placeholder="Telefone" id="telefone" required data-validation-required-message="Favor digite seu telefone.">
                             <p class="help-block text-danger"></p>
                         </div>
-                        <div class="form-group floating-label-form-group controls col-md-6">
+                        <div class="form-group floating-label-form-group controls col-md-6 col-lg-6">
                             <label for="email">WhatsApp</label>
                             <input type="tel" class="form-control" placeholder="WhatsApp" id="whatsapp" required data-validation-required-message="Favor digite seu número de WhatsApp.">
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
-                       <div class="row control-group">
-                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                      <div class="row control-group">               
+                    <div class="form-group floating-label-form-group controls col-md-6 col-lg-6">
+                            <label for="cep">CEP</label>
+                           <input type="text" class="form-control" placeholder="CEP" id="cep">
+                        </div>
+                   </div>
+                       <div class="row">
+                        <div class="form-group floating-label-form-group controls col-md-6 col-lg-6">
                             <label for="nome">Endereço</label>
                             <input type="text" class="form-control" placeholder="Endereço" id="endereco">
 
+                        </div>                    
+                        <div class="form-group floating-label-form-group controls col-md-3 col-lg-3">
+                            <label for="nome">Número</label>
+                            <input type="text" class="form-control" placeholder="Número" id="endereco">
+
                         </div>
-                    </div><br />
+                         <div class="form-group floating-label-form-group controls col-md-3 col-lg-3">
+                            <label for="nome">Compl.</label>
+                            <input type="text" class="form-control" placeholder="Compl." id="endereco">
+
+                        </div>
+                    </div>
+                        <div class="row">
+                        <div class="form-group floating-label-form-group controls col-md-6 col-lg-6">
+                            <label for="nome">Bairro</label>
+                            <input type="text" class="form-control" placeholder="Bairro" id="bairro">
+
+                        </div>                    
+                        <div class="form-group floating-label-form-group controls col-md-3 col-lg-3">
+                            <label for="nome">Cidade</label>
+                            <input type="text" class="form-control" placeholder="Cidade" id="cidade">
+
+                        </div>
+                         <div class="form-group floating-label-form-group controls col-md-3 col-lg-3">
+                            <label for="nome">UF</label>
+                            <input type="text" class="form-control" placeholder="UF" id="uf">
+
+                        </div>
+                    </div>
+                    <br />
                     <div class="row">
-                        <div class="form-group controls col-md-6">
+                        <div class="form-group controls col-md-6 col-lg-6">
                             <label for="escolaridade">Escolaridade</label>
                             <select class="form-control" name="escolaridade" id="escolaridade">
                                 <option value="0">Selecione</option>
@@ -99,7 +133,7 @@
                                 <option value="superior">Superior</option>
                             </select>
                         </div>
-                        <div class="form-group controls col-md-6">
+                        <div class="form-group controls col-md-6 col-lg-6">
                             <label for="perfil">Perfil</label>
                             <select class="form-control" name="perfil" id="perfil">
                                 <option value="0">Selecione</option>
@@ -108,27 +142,29 @@
                             </select>
                         </div>
                     </div>
-                    <div class="row control-group">
-                        <div class="form-group col-xs-12 floating-label-form-group controls">
-                            <label for="nome">Faz parte de alguma Igreja?</label>
-                            <input type="text" class="form-control" placeholder="Faz parte de alguma Igreja?" id="igreja">
+                     <div class="row">
+                        <div class="form-group floating-label-form-group controls col-md-6 col-lg-6">
+                            <label for="nome">Faz parte de alguma Igreja? Qual? Onde?</label>
+                            <input type="text" class="form-control" placeholder="Faz parte de alguma Igreja? Qual? Onde?" id="igreja">
+                        </div>                   
+                        <div class="form-group floating-label-form-group controls col-md-6 col-lg-6">
+                            <label for="nome">Voce é líder em sua igreja? Professor, Pastor ou outro?</label>
+                            <input type="text" class="form-control" placeholder="Voce é líder em sua igreja? Professor, Pastor ou outro?" id="lider">
                         </div>
-                    </div>
-                    <div class="row control-group">
-                        <div class="form-group col-xs-12 floating-label-form-group controls">
-                            <label for="nome">Voce é líder em sua igreja? Professor ou Pastor?</label>
-                            <input type="text" class="form-control" placeholder="Voce é líder em sua igreja? Professor ou Pastor?" id="lider">
+                    </div><br />
+                    <div class="row control-group">               
+                    <div class="form-group controls col-md-6 col-lg-6">
+                            <label for="saber">Como ficou sabendo de A Bíblia em Libras?</label>
+                            <select class="form-control" name="saber" id="saber">
+                                <option value="0">Selecione</option>
+                                <option value="google">Google</option>
+                                <option value="facebook">Facebook</option>
+                                <option value="internet">Internet</option>
+                            </select>
                         </div>
-                    </div>
-                    <div class="row control-group">
-                        <div class="form-group col-xs-12 floating-label-form-group controls">
-                            <label for="nome">Como ficou sabendo de A Bíblia em Libras?</label>
-                            <input type="text" class="form-control" placeholder="Como ficou sabendo de A Bíblia em Libras?" id="saber">
-                        </div>
-                    </div>
-                    <br />
+                   </div>
                     <button type="submit" class="btn btn-primary">Assinar</button>
-                    <button type="reset" class="btn btn-warning">Apagar</button>
+                    <button type="reset" class="btn btn-warning">Limpar</button>
 
                 </form>
             </div>
